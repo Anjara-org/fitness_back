de81857991946d3d43e7b5fca63f520fc2929ce5
fanambynana <hei.fanambinana@gmail.com>
2024-02-15 09:21:39 +0300

    feat : endpoint and crud for user
    feat: implement get all users
    feat: impement get user by id
    feat: implement post user
    feat: implement delete user

--- src/main/java/org/app/fitness_app/Security/ApplicationConfig.java
+++ src/main/java/org/app/fitness_app/Security/ApplicationConfig.java
@@ -2,27 +2,24 @@ package org.app.fitness_app.Security;
 
 import lombok.RequiredArgsConstructor;
 import org.app.fitness_app.Model.User;
-import org.app.fitness_app.Repository.UserInterface;
+import org.app.fitness_app.Repository.UserCrudOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.security.AuthProvider;
 import java.util.Optional;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
-    private final UserInterface repository;
+    private final UserCrudOperations repository;
     @Bean
     public UserDetailsService userDetailsService(){
         //lambda expression
--- src/main/java/org/app/fitness_app/Service/UserService.java
+++ src/main/java/org/app/fitness_app/Service/UserService.java
@@ -1,31 +1,51 @@
 package org.app.fitness_app.Service;
 
-import lombok.RequiredArgsConstructor;
 import org.app.fitness_app.Model.User;
 import org.app.fitness_app.Model.UserAuthenticate;
-import org.app.fitness_app.Repository.UserInterface;
+import org.app.fitness_app.Repository.UserCrudOperations;
+import org.aspectj.weaver.NewConstructorTypeMunger;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserService {
-    private final UserInterface userInterface;
+    private final UserCrudOperations userCrudOperations;
     private final JwtService jwtService;
+
+    public UserService(UserCrudOperations userCrudOperations, JwtService jwtService) {
+        this.userCrudOperations = userCrudOperations;
+        this.jwtService = jwtService;
+    }
     public User AddNewUser(User user )  {
-        return userInterface.save(user);
+        return userCrudOperations.save(user);
     }
     public User findByEmail(String email) {
-        return userInterface.findByEmail(email);
+        return userCrudOperations.findByEmail(email);
     }
     public String login(UserAuthenticate request) {
-        User userOptional = userInterface.findByEmail(request.getEmail());
+        User userOptional = userCrudOperations.findByEmail(request.getEmail());
         if (userOptional != null) {
-            User user = userOptional;
-            if (request.getPassword().equals(user.getPassword())) {
-                return jwtService.generateTokens(user);
+            if (request.getPassword().equals(userOptional.getPassword())) {
+                return jwtService.generateTokens(userOptional);
             }
         }
         return null;
     }
 
+    public List<User> findAll() {
+        return userCrudOperations.findAll();
+    }
+    public Optional<User> findById(int id) {
+        return userCrudOperations.findById(id);
+    }
+    public User save(User toSave) {
+        return userCrudOperations.save(toSave);
+    }
+
+    public String deleteById(int id) {
+        userCrudOperations.deleteById(id);
+        return "User deleted !";
+    }
 }
