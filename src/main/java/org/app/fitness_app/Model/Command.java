19fbac611441c725a32cc8abddd2ffeda325ef34
Faniry <faniryrandria08@gmail.com>
2024-02-17 22:26:15 +0300

    feat: create command constructor

--- src/main/java/org/app/fitness_app/Model/Command.java
+++ src/main/java/org/app/fitness_app/Model/Command.java
@@ -1,12 +1,15 @@
 package org.app.fitness_app.Model;
 
+import ch.qos.logback.classic.pattern.DateConverter;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
+
 @Entity
-@AllArgsConstructor
 @Data
 @NoArgsConstructor
 @Table(name = "command")
@@ -17,7 +20,29 @@ public class Command {
     int id;
 
     @ManyToOne
+    @JoinColumn(name = "id_user")
+    User user;
+
+    @ManyToOne
+    @JoinColumn(name = "id_training")
+    Training training;
+
+    @Column(name = "created_date", nullable = false)
+    Date createdDate;
+
+    @Column(name = "modified_date")
+    Date modified_date;
+
+    @ManyToOne
     @JoinColumn(name = "id_coach")
     Coach coach;
 
+    public Command(int id, User user, Training training, Date createdDate, Date modified_date, Coach coach) {
+        this.id = id;
+        this.user = user;
+        this.training = training;
+        this.createdDate = new Date();
+        this.modified_date = modified_date;
+        this.coach = coach;
+    }
 }
